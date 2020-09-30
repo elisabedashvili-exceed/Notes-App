a0c731d15bd7cfb9fa687cc863674c0d96649ea5
elisabedashvili-exceed <v.elisabedashvili.exceedteam@gmail.com>
2020-09-30 18:52:35 +0400

    fix

--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, "..", "public");
+const publicPath = path.join(__dirname, "..", "build");
 
 app.use(express.static(publicPath));
 
+console.log("publicPath", publicPath);
+
 app.get("*", (req, res) => {
+  console.log("publicPath", publicPath);
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
